2ea4fec796cd5577c767a099b75ca4035f5f2f77
Innectic <innectic@gmail.com>
2017-06-14 21:40:21 -0700

    Split into modules, start on Spigot plugin

---
+++ Spigot/src/main/java/me/innectic/permissify/spigot/PermissifyMain.java
@@ -0,0 +1,21 @@
+package me.innectic.permissify.spigot;
+
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author Innectic
+ * @since 6/14/2017
+ */
+public class PermissifyMain extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
