35587cf9343242b918f26abf12e0f5906e89442a
Innectic <innectic@gmail.com>
2017-08-23 01:47:37 -0700

    Put license info everywhere

--- API/src/main/java/me/innectic/permissify/api/util/ChatFormatter.java
+++ API/src/main/java/me/innectic/permissify/api/util/ChatFormatter.java
@@ -1,3 +1,27 @@
+/*
+*
+* This file is part of Permissify, licensed under the MIT License (MIT).
+* Copyright (c) Innectic
+* Copyright (c) contributors
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+ */
 package me.innectic.permissify.api.util;
 
 import me.innectic.permissify.api.PermissifyAPI;
@@ -30,7 +54,7 @@ public class ChatFormatter {
         Optional<PermissionGroup> group = handler.getPrimaryGroup(uuid);
         if (!group.isPresent()) return username + ": " + message;
         String formatter = handler.getChatFormat(false);
-        String finalGroup = group.get().getPrefix() + " " + group.get().getName() + " " + group.get().getSuffix();
+        String finalGroup = group.get().getPrefix() + group.get().getName() + group.get().getSuffix();
         String color = "&" + group.get().getChatColor();
         return formatter.replace("{group}", finalGroup)
                 .replace("{username}", username).replace("{message}", color + message);
